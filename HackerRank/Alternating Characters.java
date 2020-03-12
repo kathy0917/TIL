36e151b33fdc09999dabcc0078cebbe2266e0b44
kathy95917 <kathy95917@naver.com>
2020-03-13 02:10:25 +0900

    Interview Preparation Kit/String Manipulation/Alternating Characters

---
+++ HackerRank/Alternating Characters.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the alternatingCharacters function below.
+    static int alternatingCharacters(String s) {
+
+        String[] str = null;
+        str = s.split("");
+       
+        int a_num = 0;
+
+        for(int i=0;i<str.length-1;i++){
+            if(str[i].equals(str[i+1])){
+                a_num += 1;
+            }
+        }
+       
+            return a_num;
+
+    }
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = alternatingCharacters(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
