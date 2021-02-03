f560c250901896a62fbbe24ca6963fc16f0cf304
Nahyeon Kang <kathy95917@naver.com>
2021-02-03 16:29:41 +0900

    Delete Algorithm-Exercise directory

--- Algorithm-Exercise/Programmers/largestNum.js
+++
@@ -1,6 +0,0 @@
-function solution(numbers) {
-     let answer = numbers.sort((a,b)=>(`${b}`+`${a}`)-(`${a}`+`${b}`));
-     answer = answer[0] === 0 ? "0" : answer.join("");
-    
-    return answer;
-}
