b35a2e7377131a19e34835a0cefca99a2203f252
Nahyeon Kang <kathy95917@naver.com>
2021-03-05 19:37:49 +0900

    section8-10

--- Inflearn/2021-02-26.js
+++ Inflearn/2021-02-26.js
@@ -1,11 +1,4 @@
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>출력결과</title>
-    </head>
-    <body>
-        <script>
-            function solution(m, arr){         
+function solution(m, arr){         
                 let answer=[];
                 n=arr.length;
                 let ch=Array.from({length:n}, ()=>0);
@@ -31,45 +24,3 @@
 
             let arr=[3, 6, 9];
             console.log(solution(2, arr));
-        </script>
-    </body>
-</html>
-
-
-
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>DFS**</title>
-    </head>
-    <body>
-        <script>
-            function solution(m, arr){         
-                let answer=[];
-                n=arr.length;
-                let ch=Array.from({length:n}, ()=>0);
-                let tmp=[];
-                function DFS(L){
-                    if(L === m){
-                        answer.push(tmp.slice());
-                    }else{
-                        for(let i=0;i<n;i++){
-                            if(ch[i] === 0){
-                                ch[i] = 1;
-                                tmp[L] = arr[i];
-                                DFS(L+1);
-                                ch[i] = 0;
-                            }
-                            
-                        }
-                    }
-                }
-                DFS(0);
-                return answer;
-            }
-
-            let arr=[3, 6, 9];
-            console.log(solution(2, arr));
-        </script>
-    </body>
-</html>
